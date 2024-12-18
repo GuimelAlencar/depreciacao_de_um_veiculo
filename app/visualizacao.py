bb64d00c9c467025c721ffe2b689a24b7dd000ef
Guímel de Alencar <guimel.alencar.s@gmail.com>
2024-12-18 19:52:05 -0400

    Testes da visualizazção

--- app/visualizacao.py
+++ app/visualizacao.py
@@ -11,17 +11,42 @@ biblioteca como StreamLit, Python kivy ou Plotly Dash
 # Eles serão os únicos meios de comunicação com o back do qual vocês terão que se preocupar.
 from controle import depreciacao, financiamento, get_dados_db
 
-# testes dados_db
-tipos_de_veiculos = get_dados_db("tipos")
-print(f"Todos os tipos de veículos cadastrados no db: {tipos_de_veiculos}")
+dados_depreciacao = depreciacao(
+    #Tipo
+    "carro", 
+    # Marca
+    "toyota",
+    # Modelo
+    "corolla",
+    # Ano de fabricação
+    "2020",
+    # Tipo de consumo
+    "gasolina"
+)
 
-for tipo in tipos_de_veiculos:
-    marcas_de_um_tipo_de_veiculo = get_dados_db("marcas", tipo)
-    print(f"Todas as marcas de {tipo} cadastrados no db: {marcas_de_um_tipo_de_veiculo}")
+# Printing data
+print("Depreciação de um toyota corolla 2020: ")
+for mes, valor in dados_depreciacao:
+    print(f"{mes}º mes: R${valor}")
+print("")
 
-    for marca in marcas_de_um_tipo_de_veiculo:
-        modelos_de_uma_marca = get_dados_db("modelos", tipo, marca)
-        print(f"Todas os modelos de {tipo} da marca {marca} cadastrados no db: {modelos_de_uma_marca}")
+dados_depreciacao = depreciacao(
+    #Tipo
+    "carro", 
+    # Marca
+    "toyota",
+    # Modelo
+    "corolla",
+    # Ano de fabricação
+    "2024",
+    # Tipo de consumo
+    "gasolina"
+)
+
+# Printing data
+print("Depreciação de um toyota corolla 2024: ")
+for mes, valor in dados_depreciacao:
+    print(f"{mes}º mes: R${valor}")
 print("")
 
 # Testes financiamento
@@ -62,7 +87,7 @@ dados_do_financiamento = financiamento(
     # Modelo
     "corolla",
     # Ano de fabricação
-    "2020",
+    "2024",
     # Tipo de consumo
     "gasolina",
     # 5%
@@ -81,4 +106,17 @@ dados_do_financiamento = financiamento(
 print("Financiamento pela SAC: ")
 for parcela, valor in dados_do_financiamento:
     print(f"{parcela}º parcela: R${valor}")
+print("")
+
+# testes dados_db
+tipos_de_veiculos = get_dados_db("tipos")
+print(f"Todos os tipos de veículos cadastrados no db: {tipos_de_veiculos}")
+
+for tipo in tipos_de_veiculos:
+    marcas_de_um_tipo_de_veiculo = get_dados_db("marcas", tipo)
+    print(f"Todas as marcas de {tipo} cadastrados no db: {marcas_de_um_tipo_de_veiculo}")
+
+    for marca in marcas_de_um_tipo_de_veiculo:
+        modelos_de_uma_marca = get_dados_db("modelos", tipo, marca)
+        print(f"Todas os modelos de {tipo} da marca {marca} cadastrados no db: {modelos_de_uma_marca}")
 print("")
