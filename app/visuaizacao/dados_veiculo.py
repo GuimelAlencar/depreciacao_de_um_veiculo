e2dbd94d4477eab2653fb682627ab9167a955216
Guímel de Alencar <guimel.alencar.s@gmail.com>
2024-12-18 22:56:04 -0400

    new branch

---
+++ app/visuaizacao/dados_veiculo.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Veículo",
+    page_icon="🚗",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+
---
+++ app/visuaizacao/sobre.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Sobre",
+    page_icon="🔍",
+)
+
+st.write("# Sobre o projeto. 🔍")
+
+st.markdown(
+    """
+    O objetivo do projeto consiste em uma aplicação que demonstra de forma visual um paralelo 
+    entre um financiamento contraído para a compra de um veículo, e a desvalorização do mesmo 
+    em um período de tempo similar.
+    ### Introdução
+    O projeto visa criar uma aplicação que ajude os usuários a visualizarem o impacto da desvalorização 
+    de um veículo em relação às parcelas de um financiamento de mesmo valor, permitindo entender como o 
+    valor do carro e as parcelas se comportam ao longo do tempo. A ferramenta busca proporcionar uma visão 
+    clara e interativa para auxiliar nas decisões financeiras, ajudando os compradores a entenderem melhor 
+    o compromisso financeiro adquirido e as implicações da desvalorização do veículo.
+    ### Como utilizar ?
+    
+"""
+)
